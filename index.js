252bd7d8822227a573151b747b28ecb448a7de1e
Reuben Taveras <rtaveras824@hotmail.com>
2022-10-24 21:03:34 -0400

    Added dotenv

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
 const port = process.env.PORT || 3000;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+	.then(console.log('Connected to mongo'))
+	.catch((err) => console.error(err));
 
 app.get('/', (req, res) => {
 	res.send('Done');
